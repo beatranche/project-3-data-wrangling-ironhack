d03e86cbd6351966bad1fb4eb08794e90d46c489
beatranche <beatranche@gmail.com>
2024-09-11 18:03:06 +0200

    graphics-added

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+ 
 
 # Funtion to read dataframes fron a CSV file
 def get_dataframes_from_csv(file_path):
@@ -122,8 +123,79 @@ def convert_date_to_integer(df, column_name):
     # Return the DataFrame with the converted column
     return df
 
-# Function to create a grafic
+# Function to get continents from countries
+def get_continents(df, continent):
+    """
+    Get the continents from the given DataFrame.
 
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the countries.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A filtered DataFrame with the countries of the given continent.
+
+    Notes
+    -----
+    The function filters the given DataFrame by the given continent.
+    The continent can be either 'Europe' or 'Asia'.
+    If the continent is not one of the above, the function returns None.
+    """
+
+    # European countries
+    eu_countries = {
+        'Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic',
+        'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
+        'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta',
+        'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
+        'Spain', 'Sweden'
+    }
+
+    # Asian countries
+    asian_countries = {
+        'Armenia', 'Azerbaijan', 'Georgia', 'Kazakhstan', 'Cyprus', 'Turkey', 'Russia',
+        'Saudi Arabia', 'Iran', 'Iraq', 'Syria', 'Lebanon', 'Jordan', 'Israel', 'Palestine',
+        'United Arab Emirates', 'Oman', 'Yemen', 'Kuwait', 'Bahrain', 'Qatar', 'Afghanistan',
+        'Pakistan', 'India', 'Nepal', 'Bhutan', 'Maldives', 'Sri Lanka', 'China', 'Mongolia',
+        'North Korea', 'South Korea', 'Japan', 'Taiwan'
+    }
+
+    if continent == 'Europe':
+        # Filter European countries
+        return df[df['Country'].isin(eu_countries)]
+    elif continent == 'Asia':
+        # Filter Asian countries
+        return df[df['Country'].isin(asian_countries)]
+    else:
+        return None
+
+# Function to short columns by electric consumption
+def sort_by_electric_consumption(df):
+    """
+    Sort the given DataFrame by the 'Electricity_Consumption' column in descending order.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame to be sorted.
+
+    Returns
+    -------
+    pandas.DataFrame
+        The sorted DataFrame.
+    """
+    # Sort the DataFrame by the 'Electricity_Consumption' column in descending order
+    df = df.sort_values(by='Electricity_Consumption', ascending=False)
+
+    # Return the sorted DataFrame
+    return df
+
+
+
+# Function to create a grafic
 def create_graph(data):
     # Convert to DataFrame
     df = pd.DataFrame(data)
@@ -156,7 +228,7 @@ def main():
         'Date:Annual_Consumption_Electricity': 'Date_of_Consumption_and_Loss_Electricity',
         'Value:Annual_Consumption_Electricity': 'Electricity_Consumption',
         'Date:Annual_Loss_Electricity': 'Date_of_Loss_Electricity',  # Updated name
-        'Value:Annual_Loss_Electricity': 'Loss_electricity',
+        'Value:Annual_Loss_Electricity': 'Loss_electricity', # 
         'Date:Annual_Emissions_CarbonDioxide_ElectricityGeneration': 'Year_of_Emissions_of_CarbonDioxide_Electricity',
         'Value:Annual_Emissions_CarbonDioxide_ElectricityGeneration': 'Emissions_CarbonDioxide_ElectricityGeneration'
     }
@@ -169,9 +241,18 @@ def main():
     df_merged = drop_missing_values(df_merged)
     df_merged = rename_columns(df_merged, columns_mapping)
     df_merged = convert_date_to_integer(df_merged, columns_to_int)
-    display(df_merged.head(5))
+
     
-    create_graph(df_merged)
+    df_european_countries = get_continents(df_merged, 'Europe')
+    df_asian_countries = get_continents(df_merged, 'Asia')
+    
+    # Short results by electric consumption
+    df_european_countries = sort_by_electric_consumption(df_european_countries)
+    df_asian_countries = sort_by_electric_consumption(df_asian_countries)
+    
+    # Create graphs with the first 10  countries
+    create_graph(df_european_countries.head(10))
+    create_graph(df_asian_countries.head(10))
     
     
 
